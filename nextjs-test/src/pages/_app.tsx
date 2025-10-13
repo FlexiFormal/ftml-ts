b605c7639a2190efcdd2dfc98a4904c113df684f
abchugh <achugh89@gmail.com>
2025-10-13 13:45:08 +0530

    Initialization fix

--- nextjs-test/src/pages/_app.tsx
+++ nextjs-test/src/pages/_app.tsx
@@ -5,24 +5,19 @@ import { createContext, useState } from "react";
 
 let flamsInitialized = false;
 const initStartTime = Date.now();
-const FLAMS_URL = process.env.NEXT_PUBLIC_FLAMS_URL ?? "https://mmt.beta.vollki.kwarc.info"; // "https://mathhub.info"
+const FLAMS_URL =
+  process.env.NEXT_PUBLIC_FLAMS_URL ?? "https://mmt.beta.vollki.kwarc.info"; // "https://mathhub.info"
 // this code runs earlier if its not in the useEffect
 initialize(FLAMS_URL, "WARN")
   .then(() => {
     console.log("FTML initialized: ", Date.now() - initStartTime, "ms");
     flamsInitialized = true;
   })
-  .catch((err) => {
-    console.error(
-      `FTML initialization failed: [${process.env.NEXT_PUBLIC_FLAMS_URL}]`,
-      err
-    );
-  });
+  .catch((err) =>
+    console.error(`FTML initialization failed: [${FLAMS_URL}]`, err)
+  );
 
-
-export const ReadyToRender = createContext<boolean>(
-    false
-);
+export const ReadyToRender = createContext<boolean>(false);
 
 export default function App({ Component, pageProps }: AppProps) {
   const [readyToRender, setReadyToRender] = useState(false);
@@ -32,5 +27,11 @@ export default function App({ Component, pageProps }: AppProps) {
       clearInterval(interval);
     }
   }, 10);
-  return <ReadyToRender.Provider value={readyToRender}><Component {...pageProps} /></ReadyToRender.Provider>;
+  if (!readyToRender) return <div>Loading...</div>;
+
+  return (
+    <ReadyToRender.Provider value={readyToRender}>
+      <Component {...pageProps} />
+    </ReadyToRender.Provider>
+  );
 }
