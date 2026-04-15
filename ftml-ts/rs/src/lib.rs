e5bbccb3a628afad7d4a4b2acdc8e7e260d03f81
Jazzpirate <raupi@jazzpirate.com>
2026-04-15 14:19:53 +0200

    print library version at start

--- ftml-ts/rs/src/lib.rs
+++ ftml-ts/rs/src/lib.rs
@@ -37,5 +37,47 @@ pub fn initialize(url: Option<String>, log_level: Option<LogLevel>) {
     if let Some(url) = url {
         ftml_viewer::backend::BackendUrlRef::set_url(&url);
     }
+    leptos::logging::log!("ftml version: {VERSION}");
     ftml_viewer::init(log_level,&["ftml_ts"]);
 }
+
+static VERSION: &str = {
+    // annoyingly explicit to remain const
+    const fn string_eq(a:&str,b:&str) -> bool {
+        let mut i = 0;
+        while i < b.len() {
+            let ca = a.as_bytes()[i];
+            let cb = b.as_bytes()[i];
+            if ca != cb {return false}
+            i += 1;
+        }
+        true
+    }
+    const fn drop_n(s:&mut &str,mut curr:usize) {
+        loop {
+            if let Some((_,n)) = s.split_at_checked(curr) {
+                *s = n.trim_ascii_start();
+                return
+            }
+            curr += 1;
+        }
+    }
+    const VERSION : &str = "\"version\"";
+    let mut pkg = include_str!("../../ts/package.json");
+    while !string_eq(pkg,VERSION) {
+        drop_n(&mut pkg,1);
+    }
+    drop_n(&mut pkg,VERSION.len());
+    assert!(pkg.as_bytes()[0] == b':');
+    drop_n(&mut pkg,1);
+    assert!(pkg.as_bytes()[0] == b'"');
+    drop_n(&mut pkg,1);
+    let mut i = 0;
+    while pkg.as_bytes()[i] != b'"' {
+        i += 1;
+    }
+    let Some((ret,_)) = pkg.split_at_checked(i) else {
+        panic!("failed to parse package.json");
+    };
+    ret
+};
